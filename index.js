d4033959b40a2940cedc100a094d4d968901683b
tash02 <tash.brinkley@gmail.com>
2018-07-11 19:05:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Write your code in this file!
 
+var currentUser = "tasha"
+var welcomeMessage = "Welcome to Klossybook, " + (currentUser) + "!"
+const upperCaseCurrentUser = currentUser.toUpperCase();
+var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + (upperCaseCurrentUser) + "!"
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+var shortGreeting = "Welcome, " + (firstInitial) + "!"
